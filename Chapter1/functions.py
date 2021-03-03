1738b4305020fda9eb368802fd70830d8f9c9bd4
elvis <entirelymagic@gmail.com>
2021-03-03 13:55:41 +0200

    changed cmmdc in order to check only the lowest common denominator between 2 numbers.

--- Chapter1/functions.py
+++ Chapter1/functions.py
@@ -1,20 +1,9 @@
-def cmmdc(*args: int):
+def cmmdc(a: int, b: int):
     """Get the lowest common denominator
     """
-
-    for arg in args:
-        arg = abs(arg)
-        if len(args) > 1:
-            next_arg = args[args.index(arg)+1]
-            if arg > next_arg:
-                return cmmdc(arg-next_arg, next_arg)
-            elif next_arg > arg:
-                return cmmdc(arg, next_arg-arg)
-            else:
-                return arg
-        else:
-            return arg
-
-
-
-
+    if a > b:
+        return cmmdc(a-b, a)
+    elif a < b:
+        return cmmdc(a, b-a)
+    else:
+        return a
