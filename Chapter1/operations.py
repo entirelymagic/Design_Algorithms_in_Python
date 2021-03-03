e04d1fbd05b72a4dc54c65bee71a9fa0b364d4e7
elvis <entirelymagic@gmail.com>
2021-03-03 14:50:58 +0200

    Performed some  simple operations.

--- Chapter1/operations.py
+++ Chapter1/operations.py
@@ -6,10 +6,15 @@ from Chapter1.def_rational_numbers import Rational
 
 print(Rational.processed_numbers)
 
-f1 = Rational(1, 8)
-f2 = Rational(1, 4)
+f1 = Rational(3, 4)
+f2 = Rational(2, 9)
+c1 = Rational(23, 55)
 f3 = f1 - f2
 f4 = f1 + f2
+f5 = f2 * c1
+f6 = f1 * f2
 
+print(f3, f4, f5, f6)
+print(Rational.processed_numbers)
 
-print(f3, f4)
+# 19/36 35/36 46/495 1/6
