1cf44818a3ccaa4b1e554558d69947f62b299ee3
elvis <entirelymagic@gmail.com>
2021-03-03 14:53:21 +0200

    7. Added `__mul__` method in order to perform multiplication between Rational objects.
    Reformat file to reduce redundancy by using a irreducible_fraction method that was created.

--- Chapter1/def_rational_numbers.py
+++ Chapter1/def_rational_numbers.py
@@ -19,7 +19,7 @@ class Rational:
     @:raise Error if b==0
     """
 
-    processed_numbers = 0
+    processed_numbers: int = 0
 
     def __init__(self, a: int, b: int) -> None:
         if type(a) is not int:
@@ -41,17 +41,23 @@ class Rational:
 
         return str(self.a) + '/' + str(self.b)
 
+    @staticmethod
+    def _irreducible_fraction(a: int, b: int,) -> 'Rational':
+        """Take numerator and denominator for Rational object
+        and return it after checking if it a irreducible fraction.
+        """
+        lcd = cmmdc(a, b)
+        return Rational(a // lcd, b // lcd)
+
     def __add__(self, other_num: 'Rational') -> 'Rational':
         """Addition for 2 rational numbers(objects)"""
 
         resulted_numerator = self.a * other_num.b + self.b * other_num.a
         resulted_denominator = self.b * other_num.b
-        # get the lowest common denominator
-        lcd = cmmdc(resulted_numerator, resulted_denominator)
-        # return the rational number as a irreducible fraction
-        return Rational(int(resulted_numerator / lcd), int(resulted_denominator / lcd))
+        # return a Rational number after it is checked that it is not a irreducible_fraction
+        return self._irreducible_fraction(resulted_numerator, resulted_denominator)
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Rational') -> bool:
         """ Overload default __eq__ method.
         Test to see if 2 objects Rational are equal"""
 
@@ -59,12 +65,21 @@ class Rational:
         numerator2 = other.a * self.b
         return numerator1 == numerator2
 
-    def __sub__(self, other):
+    def __sub__(self, other: 'Rational') -> 'Rational':
         """Overload default __sub__ method
         Perform subtraction between 2 Rational objects
         """
         resulted_numerator = self.a * other.b - self.b * other.a
         resulted_denominator = self.b * other.b
-        # get lowest common denominator
-        lcd = cmmdc(resulted_numerator, resulted_denominator)
-        return Rational(resulted_numerator//lcd, resulted_denominator//lcd)
+        # return a Rational number after it is checked that it is not a irreducible_fraction
+        return self._irreducible_fraction(resulted_numerator, resulted_denominator)
+
+    def __mul__(self, other: 'Rational'):
+        """Overload default __sub__ method
+        Perform Multiplication between 2 Rational objects
+        """
+        resulted_numerator = self.a * other.a
+        resulted_denominator = self.b * other.b
+        # return a Rational number after it is checked that it is not a irreducible_fraction
+        return self._irreducible_fraction(resulted_numerator, resulted_denominator)
+
