5fd2567c97ad86085082b3d63442570a24de0de6
elvis <entirelymagic@gmail.com>
2021-03-03 12:35:10 +0200

    Added test case to see if 2 different Rational object are equal;

---
+++ Chapter1/tests/test_chapter1_test.py
@@ -0,0 +1,29 @@
+import pytest
+from Chapter1.def_rational_numbers import Rational
+
+
+class TestRationalClass:
+    r1 = Rational(1, 8)
+    r2 = Rational(1, 4)
+    r3 = Rational(2, 16)
+
+    @pytest.fixture()
+    def rational(self):
+        rational1 = Rational(1, 2)
+        return rational1
+
+    def test_error_0_as_denominator(self):
+        """Test if given 0 as denominator for Rational Object raises an Error."""
+        with pytest.raises(Exception):
+            Rational(3, 0)
+
+    def test_rational_numbers_addition(self, rational):
+        """Test if 2 rational numbers are returning the correct print value"""
+        r3 = self.r1 + self.r2
+        assert r3.__str__() == '3/8'
+
+    def test_rational_number_equality(self):
+        assert self.r1 == self.r3
+
+
+
