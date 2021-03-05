f7c3a2e187cc79c17e7030041531348e04ed4e4c
elvis <entirelymagic@gmail.com>
2021-03-05 18:15:43 +0200

    operations.py

--- Chapter1/operations.py
+++ Chapter1/operations.py
@@ -3,17 +3,10 @@ from Chapter1.def_rational_numbers import Rational
 
 """Operations performed with rational numbers"""
 
+r1 = Rational(1, 8)
+r2 = Rational(1, 4)
+r3 = Rational(1, 16)
 
-f1 = Rational(3, 4)
-f2 = Rational(2, 9)
-r1 = f1 * f2
 
-print(r1)
-print(Rational.processed_numbers)
+print(r1*r2)
 
-f1 = Rational(1, 8)
-f2 = Rational(1, 4)
-
-r2 = f1 / f2
-r3 = f2 / f1
-print(r2, r3)
