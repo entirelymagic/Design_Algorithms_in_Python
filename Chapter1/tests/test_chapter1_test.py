9810ff65847242ec5a4e060c3713eefd71515ea6
elvis <entirelymagic@gmail.com>
2021-03-05 18:05:31 +0200

    Refactoring to use pytest.fixture 3 rational numbers r1,r2,r3

--- Chapter1/tests/test_chapter1_test.py
+++ Chapter1/tests/test_chapter1_test.py
@@ -3,14 +3,21 @@ from Chapter1.def_rational_numbers import Rational
 
 
 class TestRationalClass:
-    r1 = Rational(1, 8)
-    r2 = Rational(1, 4)
-    r3 = Rational(2, 16)
-    Rational.processed_numbers -= 3
 
-    @pytest.fixture()
-    def rational(self):
-        pass
+    @pytest.fixture
+    def r1(self):
+        r1 = Rational(1, 8)
+        return r1
+
+    @pytest.fixture
+    def r2(self):
+        r2 = Rational(1, 4)
+        return r2
+
+    @pytest.fixture
+    def r3(self):
+        r3 = Rational(1, 8)
+        return r3
 
     def test_value_errors(self):
         """
@@ -32,45 +39,54 @@ class TestRationalClass:
         with pytest.raises(ValueError):
             Rational(5.0, 5)
 
-    def test_rational_numbers_addition(self, rational):
+    def test_rational_numbers_addition(self, r1, r2):
         """Test if 2 rational numbers are returning the correct print value"""
-        r3 = self.r1 + self.r2
+        r3 = r1 + r2
         assert r3.__str__() == '3/8'
 
-    def test_rational_number_equality(self):
+    def test_rational_number_equality(self, r1, r2, r3):
         """Test to see comparison between 2 Rational objects"""
-        assert self.r1 == self.r3
-        assert self.r1 != self.r2
+        assert r1 == r3
+        assert r1 != r2
 
-    def test_rational_number_subtraction(self):
+    def test_rational_number_subtraction(self, r1, r2):
         """Test __sub__ method for Rational objects"""
-        assert self.r1 - self.r2
+        result = r1 - r2
+        assert result.__str__() == '-1/8'
 
-    def test_rational_number_multiplication(self):
+    def test_rational_number_multiplication(self, r1, r2):
         """test __mul__  method for Rational objects"""
-        assert self.r1 * self.r2
+        result = r1 * r2
+        assert result.__str__() == '1/32'
 
-    def test_rational_number_division(self):
+    def test_rational_number_division(self, r1, r2):
         """test __truediv__  method for Rational objects"""
-        assert self.r1 / self.r2
+        result = r1 / r2
+        assert result == 0.5
 
-    def test_rational_number_floor_division(self):
+    def test_rational_number_floor_division(self, r1, r2):
         """test __floordiv__  method for Rational objects"""
-        assert self.r1 // self.r2
+        assert r1 // r2
 
-    def test_rational_number_iadd(self):
+    def test_rational_number_iadd(self, r1, r2):
         """test __iadd__  method for Rational objects"""
-        assert  self.r1 += self.r2
+        r1 = Rational(1, 8)
+        r1 += r2
+        assert r1.__str__() == "3/8"
 
-    def test_rational_number_isub(self):
+    def test_rational_number_isub(self, r1, r2):
         """test __isub__  method for Rational objects"""
-        assert  self.r1 -= self.r2
+        r1 = Rational(1, 8)
+        r1 -= r2
+        assert r1.__str__() == "-1/8"
 
-    def test_rational_number_imul(self):
+    def test_rational_number_imul(self, r1, r2):
         """test __imul__  method for Rational objects"""
-        assert  self.r1 *= self.r2
+        r1 = Rational(1, 8)
+        r1 *= r2
+        assert r1.__str__() == "1/32"
 
-    def test_rational_number_ifloordiv(self):
-        """test __imul__  method for Rational objects"""
-        assert  self.r1 //= self.r2
-
+    def test_rational_number_ifloordiv(self, r1, r2):
+        """test __ifloordiv__  method for Rational objects"""
+        r1 //= r2
+        assert r1.__str__() == "1/2"
