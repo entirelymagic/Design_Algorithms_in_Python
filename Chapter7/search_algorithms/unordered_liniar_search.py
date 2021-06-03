261f3820dbfb34f7cd8a334bc6463a93c8d5c9ce
entirelymagic@gmail.com <@lexandr@123>
2021-06-03 10:36:30 +0300

    Added more description.

--- Chapter7/search_algorithms/unordered_liniar_search.py
+++ Chapter7/search_algorithms/unordered_liniar_search.py
@@ -1,5 +1,7 @@
 def search_unordered_list(list1, searched_values):
-    """search an unordered list"""
+    """Search an unordered list
+    Complexity for the algorithm is 'O(n)' and it is resulted from the 'for loop' and the length 'n' of the list.
+    """
     for i in range(len(list1)):
         if searched_values == list1[i]:
             return list1[i]
