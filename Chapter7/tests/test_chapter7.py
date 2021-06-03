554f00ddfb9ccce9dd3e33a4baf5f72d04b456d6
entirelymagic@gmail.com <@lexandr@123>
2021-06-03 10:49:33 +0300

    Added simple ordered list search tests.

--- Chapter7/tests/test_chapter7.py
+++ Chapter7/tests/test_chapter7.py
@@ -11,8 +11,18 @@ class TestUnorderedListSearch:
         return list1
 
     def test_finding_element_in_unordered_list(self, ul1):
-        assert search_unordered_list(ul1, 14) == 14
+        assert search_unordered_list(ul1, 14) == 1
         assert search_unordered_list(ul1, 500) is None
 
 
+class TestOrderedListSearch:
 
+    @pytest.fixture
+    def ol1(self):
+        list1 = [-21, -10, 2, 14, 15, 43, 432]
+        return list1
+
+    def test_finding_element_in_ordered_list(self, ol1):
+        assert search_unordered_list(ol1, 14) == 3
+        assert search_unordered_list(ol1, 432) == 6
+        assert search_unordered_list(ol1, 500) is None
