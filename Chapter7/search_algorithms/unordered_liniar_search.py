742d8f2a8b18ab27fa4d200100273f29716ff613
entirelymagic@gmail.com <@lexandr@123>
2021-06-03 10:49:47 +0300

    Added simple ordered list search.

--- Chapter7/search_algorithms/unordered_liniar_search.py
+++ Chapter7/search_algorithms/unordered_liniar_search.py
@@ -1,9 +1,27 @@
-def search_unordered_list(list1, searched_values):
+def search_unordered_list(list1, searched_value):
     """Search an unordered list
     Complexity for the algorithm is 'O(n)' and it is resulted from the 'for loop' and the length 'n' of the list.
     """
+
     for i in range(len(list1)):
-        if searched_values == list1[i]:
-            return list1[i]
+        if searched_value == list1[i]:
+            return i
     return None
+
+
+def search_ordered_list(list1, searched_value):
+    """Search an ordered list
+    Complexity for the algorithm is 'O(n)' and it is resulted from the 'for loop' and the length 'n' of the list.
+    """
+
+    if searched_value < list1[0] or searched_value > list1[-1]:
+        return None
+    for i in range(len(list1)):
+        if searched_value == list1[i]:
+            return i
+        elif searched_value < list1[i]:
+            return None
+    return None
+
+
 
